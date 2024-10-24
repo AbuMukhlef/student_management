957b1104a1cea06d06900927fa4d432b4fc3bfc7
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-10-24 11:56:25 +0300

    good

--- lib/model/cubit/manag_stu_cubit.dart
+++ lib/model/cubit/manag_stu_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:student_management/model/student.dart';
 import '../student_manager.dart';
 
 part 'manag_stu_state.dart';
@@ -7,15 +8,19 @@ part 'manag_stu_state.dart';
 class ManagStuCubit extends Cubit<ManagStuState> {
   ManagStuCubit() : super(ManagStuInitial());
 
+  List<Student> get students => StudentManager.students;
+
   void addStudent({required String name}) {
     emitLoading();
     StudentManager.addStudent(name: name);
+    print('Student Added');
     emitUpdate();
   }
 
   void removeStudent({required String id}) {
     emitLoading();
     StudentManager.removeStudent(id: id);
+    print('Student Removed');
     emitUpdate();
   }
 
--- lib/screen/student_screen.dart
+++ lib/screen/student_screen.dart
@@ -1,61 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../model/cubit/manag_stu_cubit.dart';
 import '../model/student_manager.dart';
 
 class StudentScreen extends StatelessWidget {
   StudentScreen({super.key});
-  final StudentManager _studentManager = StudentManager();
   final TextEditingController _studentNameController = TextEditingController();
+  final TextEditingController _studentIdController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Student Management'),
-      ),
-      body: Center(
-        child: ListView(children: [
-          const SizedBox(height: 300),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-                controller: _studentNameController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Student Name',
-                )),
+    return BlocProvider(
+      create: (context) => ManagStuCubit(),
+      child: Builder(builder: (context) {
+        final cubit = context.read<ManagStuCubit>();
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Student Management'),
           ),
-          const SizedBox(height: 25),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-                controller: _studentNameController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Student Id',
-                )),
+          body: Center(
+            child: ListView(children: [
+              const SizedBox(height: 300),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                    controller: _studentNameController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Student Name',
+                    )),
+              ),
+              const SizedBox(height: 25),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                    controller: _studentIdController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Student Id',
+                    )),
+              ),
+              const SizedBox(height: 25),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton.icon(
+                      onPressed: () {
+                        cubit.removeStudent(id: _studentIdController.text);
+                      },
+                      icon: const Icon(Icons.remove_circle),
+                      label: const Text('Remove Student')),
+                  const SizedBox(width: 25),
+                  ElevatedButton.icon(
+                      onPressed: () {
+                        cubit.addStudent(name: _studentNameController.text);
+                      },
+                      label: const Text('Add Student'),
+                      icon: const Icon(Icons.add_circle)),
+                ],
+              ),
+            ]),
           ),
-          const SizedBox(height: 25),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton.icon(
-                  onPressed: () {
-                    print(_studentNameController.text);
-                  },
-                  icon: const Icon(Icons.remove_circle),
-                  label: const Text('Remove Student')),
-              const SizedBox(width: 25),
-              ElevatedButton.icon(
-                  onPressed: () {
-                    print(_studentNameController.text);
-                  },
-                  label: const Text('Add Student'),
-                  icon: const Icon(Icons.add_circle)),
-            ],
-          ),
-        ]),
-      ),
+        );
+      }),
     );
   }
 }
